e51488ca682e2ebfc166b7153ea3de09edfc0b5f
Sung Chan <102793335+chomul@users.noreply.github.com>
2026-05-26 21:46:20 +0900

    [level 3] Title: 표 편집, Time: 32.23 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.cpp
+++ 프로그래머스/3/81303. 표 편집/표 편집.cpp
@@ -3,27 +3,30 @@
 #include <stack>
 
 using namespace std;
-struct Node
-{
+
+struct Node{
+
+    int data;
     Node* next;
     Node* prev;
-    int data;
+
 };
 
 string solution(int n, int k, vector<string> cmd) {
-    string answer;
-    stack<int> deleted;
-    
+    string answer = "";
+    stack<int> deleted; 
+
     vector<Node*> node(n);
-    for(int i = 0; i < n; i++){
+
+    for(int i = 0; i < n; i++)
+    {
         node[i] = new Node();   
         node[i]->data = i;
     }
     
-    Node* curr = node[k];
-    vector<bool> remained(n, true);
     
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++)
+    {
         if(i > 0){
             node[i]->prev = node[i - 1];
         }
@@ -31,35 +34,66 @@ string solution(int n, int k, vector<string> cmd) {
             node[i]->next = node[i + 1];    
         }
     }
+    
+    
+    Node* curr = node[k];
+		// 정답 출력용 bool 배열
+    vector<bool> remained(n, true);
 
-    for (const string& c : cmd) {
-        if (c[0] == 'U') {
-            int move = stoi(c.substr(2));
-            while(move-- > 0) curr = curr->prev;            
-        } else if (c[0] == 'D') {
-            int move = stoi(c.substr(2));
-            while(move-- > 0) curr = curr->next;      
-        } else if (c[0] == 'C') {
+    for(int i = 0; i < cmd.size(); i++){
+        if(cmd[i][0] == 'U'){
+            int move = stoi(cmd[i].substr(2));
+            while(move-- > 0){
+                curr = curr->prev;
+            }
+        }else if(cmd[i][0] == 'D'){
+            int move = stoi(cmd[i].substr(2));
+            while(move-- > 0){
+                curr = curr->next;
+            }
+        }else if(cmd[i][0] == 'C'){
+
             deleted.push(curr->data);
             remained[curr->data] = false;
 
-            if (curr->prev) curr->prev->next = curr->next;
-            if (curr->next) curr->next->prev = curr->prev;
+            if(curr->prev){
+                curr->prev->next = curr->next;
+            }
+            if(curr->next){
+                curr->next->prev = curr->prev;
+            }
 
-            if (curr->next) curr = curr->next;
-            else curr = curr->prev;
-        } else if (c[0] == 'Z') {
-            int index = deleted.top();
-            remained[deleted.top()] = true;
+            if(curr->next){
+                curr = curr->next;
+            }else{
+                curr = curr->prev;
+            }
+
+        }else if(cmd[i][0] == 'Z'){
+            int row = deleted.top();
             deleted.pop();
-
-            if (node[index]->prev) node[index]->prev->next = node[index];
-            if (node[index]->next) node[index]->next->prev = node[index];
+            remained[row] = true;
+            if(node[row]->prev){
+                node[row]->prev->next = node[row];
+            }
+            if(node[row]->next){
+                node[row]->next->prev = node[row];
+            }
         }
     }
 
-    for (auto remide : remained)
-        answer += remide ? 'O': 'X';
+    for(int i = 0; i < remained.size(); i++){
+        if(remained[i]){
+            answer += 'O';
+        }else{
+            answer += 'X';
+        }
+    }
+
+    
+    
+    
+    
     
     return answer;
 }
