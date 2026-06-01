0ab6d18a205fd45b8d9589440937a30fa9570c2f
Sung Chan <102793335+chomul@users.noreply.github.com>
2026-06-01 21:01:06 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.01 ms
+메모리: 4.07 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 01일 14:51:46
+2026년 06월 01일 21:00:59
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -2,23 +2,16 @@
 
 using namespace std;
 
-
-int win(int node)
-{
-    if (node % 2 == 0) return node / 2;
-    else return node / 2 + 1;
-}
-
 int solution(int n, int a, int b)
 {
     int answer = 0;
 
-    int node1 = a, node2 = b;
-    while (node1 != node2)
+    while(a != b)
     {
+        a = (a + 1) / 2;
+        b = (b + 1) / 2;
         answer++;
-        node1 = win(node1);
-        node2 = win(node2);
     }
+    
     return answer;
 }
